dfdd3b0b0989578fd45fd60df4b0b7d3a3f562aa
Pham-Quynh <Phampt987321@gmail.com>
2026-06-14 13:32:52 +0700

    xong

--- src/Tro88.Infrastructure/obj/Debug/net8.0/Tro88.Infrastructure.AssemblyInfo.cs
+++ src/Tro88.Infrastructure/obj/Debug/net8.0/Tro88.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tro88.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7c0422cbc5d1aa7c3d025592ed31733b0424d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e155895b9a738b0a557b86c9819bdda5e38783")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tro88.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tro88.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
